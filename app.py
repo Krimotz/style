f462505e0a8ffaf653d885528cccc1e169cfc990
Krimotz <54431980+Krimotz@users.noreply.github.com>
2025-06-20 11:21:16 +0200

    Primary files upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+functions = [
+    "Library", "Power Plant", "Train Station", "Observatory", "School",
+    "Market Hall", "Water Tower", "Shrine", "Data Center", "Kindergarten"
+]
+
+styles = [
+    "Brutalism", "Gothic", "Baroque", "Art Deco", "Japanese Metabolism",
+    "Neo-futurism", "Khmer Temple", "Incan Fortress", "Nordic Vernacular",
+    "Byzantine Basilica"
+]
+
+materials = [
+    "Glass", "Mycelium", "Concrete", "Black Volcanic Stone", "Carbon Fiber",
+    "Sugar Glass", "Recycled Plastic", "Polished Marble", "Organic Ceramic",
+    "Algae-based BioBrick"
+]
+
+@app.route("/")
+def index():
+    func = random.choice(functions)
+    style = random.choice(styles)
+    material = random.choice(materials)
+    return render_template("index.html", func=func, style=style, material=material)
+
+if __name__ == "__main__":
+    app.run(debug=True)
